4873104cd6eb4e2c1e0da098a42b2582348e2efc
Erfan_akhavanrad <50219404+erfanakhavanrad@users.noreply.github.com>
2021-09-02 01:08:11 +0430

    1.0

--- src/main/java/finalsection/Main.java
+++ src/main/java/finalsection/Main.java
@@ -1,9 +1,28 @@
 package finalsection;
 
+import java.util.function.Consumer;
+
 public class Main {
 
   public static void main(String[] args) {
-    
+
   }
 
+  static void hello(String firstName, String lastName, Consumer<String> callback) {
+    System.out.println(firstName);
+    if (lastName != null) {
+      System.out.println(lastName);
+    } else {
+      callback.accept(firstName);
+    }
+  }
+
+  static void hello2(String firstName, String lastName, Runnable callback) {
+    System.out.println(firstName);
+    if (lastName != null) {
+      System.out.println(lastName);
+    } else {
+      callback.run();
+    }
+  }
 }
